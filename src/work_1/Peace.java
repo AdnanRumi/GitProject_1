9b3bf42786cb9338cd21964d849c2313950cfc9b
AdnanRumi <mugterib@gmail.com>
2021-06-13 18:00:19 +0200

    Commit segundo

--- src/work_1/Peace.java
+++ src/work_1/Peace.java
@@ -5,5 +5,7 @@ public class Peace {
     public static void main(String[] args) {
 
         System.out.println("In the beginning there was peace...");
+
+        System.out.println("AR to Git.Stop");
     }
 }
