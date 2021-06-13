5000b07d300bc10fd0de6ed67aedcff795e24953
AdnanRumi <mugterib@gmail.com>
2021-06-13 18:06:05 +0200

    Commit yet again

--- src/work_1/Peace.java
+++ src/work_1/Peace.java
@@ -4,8 +4,10 @@ public class Peace {
 
     public static void main(String[] args) {
 
-        System.out.println("In the beginning there was peace...");
+        System.out.println("Peace...");
 
         System.out.println("AR to Git.Stop");
+
+        System.out.println("Be the change you want to see in the world ");
     }
 }
